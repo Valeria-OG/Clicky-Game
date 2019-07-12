26584b693b7c61fc7ab0d3b80e614daa3b9f7e27
Valeria <valeria.obreguillermo@yahoo.com>
2019-07-11 23:56:01 -0500

    part of the code done

---
+++ src/components/card/card.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./Card.css";
+
+
+
+const Card = props => (
+
+  <div className="card" onClick={() => props.clickCount(props.id)}>
+
+    <div className="img-container">
+
+      <img alt={props.name} src={props.image} />
+
+    </div>
+
+  </div>
+
+);
+
+
+
+export default Card;
